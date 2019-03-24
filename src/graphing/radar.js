eb5000be1ccf9a4f817e967b5c20ed6566e5bdfb
Ken Kustian <kustian@mitrais.com>
2019-03-24 13:52:33 +0700

    Adjust header's cosmetic

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -508,9 +508,7 @@ const Radar = function (size, radar) {
       .attr('class', 'footer-content')
       .append('p')
       .html('Powered by <a href="https://www.thoughtworks.com"> ThoughtWorks</a>. ' +
-      'By using this service you agree to <a href="https://www.thoughtworks.com/radar/tos">ThoughtWorks\' terms of use</a>. ' +
-      'You also agree to our <a href="https://www.thoughtworks.com/privacy-policy">privacy policy</a>, which describes how we will gather, use and protect any personal data contained in your public Google Sheet. ' +
-      'This software is <a href="https://github.com/thoughtworks/build-your-own-radar">open source</a> and available for download and self-hosting.')
+      'This software is <a href="https://github.com/mitrais/build-your-own-radar">open source</a> and available for download and self-hosting.')
   }
 
   function mouseoverQuadrant (order) {
